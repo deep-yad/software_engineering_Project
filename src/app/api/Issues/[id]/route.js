92baefc99fd2957166ee8af98909ae4678b30c59
Aviral-Sharma-2578 <aviralshar10@gmail.com>
2024-02-16 10:21:51 +0530

    issue controllers

---
+++ src/app/api/Issues/[id]/route.js
@@ -0,0 +1,39 @@
+import Issue from '@/models/issue'
+import { connectToDB } from "@/utils/database";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+    const { id } = params;
+    const foundIssue = await Issue.findOne({ _id: id });
+    return NextResponse.json({ foundIssue }, { status: 200 });
+}
+
+export async function PUT(req, { params }) {
+    try {
+      const { id } = params;
+  
+      const body = await req.json();
+      const issueData = body.formData;
+  
+      const updateIssueData = await Issue.findByIdAndUpdate(id, {
+        ...issueData,
+      });
+  
+      return NextResponse.json({ message: "issue updated" }, { status: 200 });
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+}
+
+export async function DELETE(req, { params }) {
+    try {
+      const { id } = params;
+  
+      await Issue.findByIdAndDelete(id);
+      return NextResponse.json({ message: "Issue Deleted" }, { status: 200 });
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ message: "Error", error }, { status: 500 });
+    }
+}
