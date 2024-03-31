ac43ced04fd6431a63e5fd8976e540c9ea750af2
Aviral-Sharma-2578 <117987884+Aviral-Sharma-2578@users.noreply.github.com>
2024-03-31 15:04:51 +0530

    Update page.jsx

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,1 +1,49 @@
 
+"use client"
+import React, { useEffect, useState } from 'react'
+import SearchPerson from '../(components)/SearchPerson';
+import SearchMachine from '../(components)/SearchMachine';
+const page = () => {
+  
+  let [personData, setPersonData] = useState([]);
+  let [machineData, setMachineData] = useState([]);
+
+
+
+
+    useEffect(() => {
+      const getMachine = async () => {
+        const response = await fetch("/api/machine", {
+          method: "GET",
+        });
+
+        const data = await response.json();
+        machineData = data;
+        setMachineData(machineData);
+        console.log(machineData);
+      };
+      
+      const getPersons = async () => {
+        const response = await fetch("/api/Persons", {
+          method: "GET",
+        });
+        
+        const data = await response.json();
+        personData = data;
+        setPersonData(personData);
+        // console.log(personData);
+      };
+      getMachine();
+      getPersons();
+    }, []);
+  return (
+    <div>
+      <h1>My Search Page</h1>
+      <SearchPerson data={personData} tag={"Search Person Data"} />
+      <SearchMachine data={machineData} tag={"Search Machine Data"} />
+    </div>
+  );
+};
+
+
+export default page
