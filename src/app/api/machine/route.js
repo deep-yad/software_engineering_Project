1635fda25e32d4aaf44aaac5bd9e9cc8599383f7
Aviral-Sharma-2578 <117987884+Aviral-Sharma-2578@users.noreply.github.com>
2024-03-31 14:51:16 +0530

    Update route.js

--- src/app/api/machine/route.js
+++ src/app/api/machine/route.js
@@ -3,7 +3,6 @@ import { connectToDB } from "@/utils/database";
 import { NextResponse } from "next/server";
 export const dynamic = "force-static";
 
-
 export async function GET() {
   try {
     const machine = await Machine.find();
@@ -17,45 +16,18 @@ export async function GET() {
 export async function POST(req) {
   try {
     console.log("lol");
-    const {
-      machineName,
-      description,
-      totalQuantity,
-      availableQuantity,
-      hasSubparts,
-      subParts,
-    } = await req.json();
-    const con = await connectToDB();
-    console.log("connected");
-
+    const body = await req.json();
     console.log("problem");
-    console.log("Machine Name:", machineName);
-    console.log("Description:", description);
-    console.log("Total Quantity:", totalQuantity);
-    console.log("Available Quantity:", availableQuantity);
-    console.log("Has Subparts:", hasSubparts);
-    //console.log("Subparts:", subParts);
-    const subPartsData = subParts.map(subPart => ({
-      machine_id: subPart
-    }));
-    console.log(subPartsData)
-    const newMachine = new Machine({
-      machine_id: 13,
-      machine_name: machineName,
-      description: description,
-      total_quantity: totalQuantity,
-      available_quantity: availableQuantity,
-      subparts:subPartsData,
-        current: [{ issue_id: 0 }], // Initialize with empty objects containing issue_id
-        completed: [{ issue_id: 1 }]
-    });
-    await newMachine.save();
-    return new Response(JSON.stringify(newMachine), { status: 201 });
+    const machineData = body.formData;
+
+    const result = await Machine.create(machineData);
+
+    return NextResponse.json(
+      { message: "machine Created", id: result._id },
+      { status: 201 }
+    );
   } catch (err) {
     console.log(err);
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 }
-
-
-
