41c9bfd6af1b2f6ca61feda1eb9b9df8d7579e30
Aviral-Sharma-2578 <117987884+Aviral-Sharma-2578@users.noreply.github.com>
2024-04-12 15:02:08 +0530

    Update page.js

--- src/app/(person)/showPerson/page.js
+++ src/app/(person)/showPerson/page.js
@@ -263,10 +263,17 @@ const page = () => {
                     </div>
                   </th>
                   <td className="px-6 py-4">
-                    <span className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
-                      <span className="h-1.5 w-1.5 rounded-full bg-green-600"></span>
-                      Cleared
-                    </span>
+                    {person.current.length > 0 ? (
+                      <span className="inline-flex items-center gap-1 rounded-full bg-orange-50 px-2 py-1 text-xs font-semibold text-orange-600">
+                        <span className="h-1.5 w-1.5 rounded-full bg-orange-600"></span>
+                        Pending
+                      </span>
+                    ) : (
+                      <span className="inline-flex items-center gap-1 rounded-full bg-green-50 px-2 py-1 text-xs font-semibold text-green-600">
+                        <span className="h-1.5 w-1.5 rounded-full bg-green-600"></span>
+                        Cleared
+                      </span>
+                    )}
                   </td>
                   <td className="px-6 py-4">{person.mobile_number}</td>
                   <td className="px-3 py-4 ">
